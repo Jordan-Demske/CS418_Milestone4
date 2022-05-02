9870176efc154e570c78220e6c6d68849dd4ab08
bringobrongo <wildloudnproud@gmail.com>
2022-05-02 02:21:21 -0400

    Fixed ais importing

--- MySQL_DAO.py
+++ MySQL_DAO.py
@@ -1,6 +1,9 @@
 import json
+import math
+
 import mysql.connector
 from mysql.connector import errorcode
+import dateutil.parser
 
 import configparser
 
@@ -39,71 +42,120 @@ class MySQLCursorManager:
 
 
 class MySQL_DAO:
-    
-    def __insert_ais_batch__(self):
+    ais_parameters = ['Class', 'MMSI']
+    static_data_parameters = ['CallSign', 'Name', 'VesselType', 'CargoType', 'Length', 'Breadth', 'Draught',
+                              'Destination', 'DestinationId']
+    position_report_parameters = ['RoT', 'SoG', 'CoG', 'Heading', 'Status']
+    position_parameters = ['type', 'coordinates']
+
+    def format_ais_message(self, msg):
+        if "Timestamp" in msg:
+            date = dateutil.parser.isoparse(msg['Timestamp'])
+            timestamp = date.strftime("%Y-%m-%d %H:%M:%S")
+            msg['Timestamp'] = timestamp
+        else:
+            msg['Timestamp'] = None
+        if "IMO" not in msg or msg['IMO'] == 'Unknown':
+            msg['IMO'] = None
+        for parameter in self.ais_parameters:
+            if parameter not in msg:
+                msg[parameter] = None
+        return msg
+
+    def format_position_report(self, msg):
+        if "Position" in msg:
+            for parameter in self.position_parameters:
+                if parameter not in msg['Position']:
+                    msg['Position'] = None
+        if "Status" not in msg or msg['Status'] == "Unknown value":
+            msg['Status'] = None
+        for parameter in self.position_report_parameters:
+            if parameter not in msg:
+                msg[parameter] = None
+        return msg
+
+    def format_static_data(self, msg):
+        if 'ETA' in msg:
+            date = dateutil.parser.isoparse(msg['ETA'])
+            msg['ETA'] = date.strftime("%Y-%m-%d %H:%M:%S")
+        else:
+            msg['ETA'] = None
+        for parameter in self.static_data_parameters:
+            if parameter not in msg:
+                msg[parameter] = None
+        return msg
+
+    def insert_ais_batch(self, filename):
+        count = 0
+        with open(filename, 'r') as file:
+            data = json.load(file)
+            for msg in data:
+                result = self.insert_ais_message(msg)
+                if json.loads(result)['success'] == 1:
+                    count += 1
+        return json.dumps({"inserts": count})
+
+    def insert_ais_message(self, msg):
         try:
             with MySQLConnectionManager() as con:
-                with MySQLCursorManager( con ) as cursor:
-                    with open('test_input.json', 'r') as file:
-                        data = json.load(file)
-                        for msg in data:
-                            date = parser.isoparse(msg['Timestamp'])
-                            
-                            timestamp = date.strftime("%Y-%m-%d %H:%M:%S")
-                            mmsi = msg['MMSI']
-                            vessel_class = msg['Class']
+                with MySQLCursorManager(con) as cursor:
+                    msg = self.format_ais_message(msg)
 
-                            stmt = """INSERT INTO ais_message(Timestamp, MMSI, Class) VALUES(%s, %s, %s);""" 
-                            cursor.execute(stmt, (timestamp, mmsi, vessel_class))
-                            con.commit()
-                            
-                            msgType = msg['MsgType']
-                            
-                            if msgType == 'position_report':
-                                
-                                nav_status = None
-                                if "NavigationalStatus" in msg:
-                                    nav_status = msg["NavigationalStatus"]
-                                lat = msg['Position']['coordinates'][0]
-                                long = msg['Position']['coordinates'][1]
-                                rot = msg['RoT']
-                                sog = msg['SoG']
-                                cog = msg['CoG']
-                                heading = msg['Heading']
-                                mv1 = 1
-                                mv2 = None
-                                mv3 = None
-                                
-                                stmt = """INSERT INTO position_report(AISMessage_Id, NavigationalStatus, Longitude, Latitude, RoT, SoG, CoG, Heading, LastStaticData_Id, MapView1_Id, MapView2_Id, MapView3_Id)
-                                          VALUES(LAST_INSERT_ID(), %s, %s, %s, %s, %s, %s, %s, (SELECT max(STATIC_DATA.AISMessage_ID) FROM STATIC_DATA, AIS_MESSAGE WHERE STATIC_DATA.AISMessage_Id = AIS_MESSAGE.Id AND AIS_MESSAGE.MMSI = %s), %s, %s, %s);""" 
-                                cursor.execute(stmt, (nav_status, lat, long, rot, sog, cog, heading, mmsi, mv1, mv2, mv3))
+                    stmt = """INSERT INTO AIS_MESSAGE(Timestamp, MMSI, Class) VALUES(%s, %s, %s);"""
+                    cursor.execute(stmt, (msg['Timestamp'], msg['MMSI'], msg['Class']))
+                    con.commit()
+
+                    if "MsgType" not in msg:
+                        return
+
+                    if msg['MsgType'] == 'position_report':
+                        msg = self.format_position_report(msg)
+                        map_views = [None, None, None]
+                        if msg['Position'] is not None:
+                            for i in range(len(map_views)):
+                                map_view_dict = self.get_tile(i + 1, msg['Position']['coordinates'][1],
+                                                              msg['Position']['coordinates'][0])
+                                cursor.execute(
+                                    """SELECT * from MAP_VIEW WHERE LongitudeW = %s AND LongitudeE = %s AND LatitudeN = %s AND LatitudeS = %s;""",
+                                    (map_view_dict['west'], map_view_dict['east'], map_view_dict['north'],
+                                    map_view_dict['south']))
+                                rs = cursor.fetchone()
+
+                                if rs is None:
+                                    continue
+                                else:
+                                    map_views[i] = rs[0]
+
+                        stmt = """INSERT INTO POSITION_REPORT(AISMessage_Id, NavigationalStatus, Longitude, Latitude, RoT, SoG, CoG, Heading, LastStaticData_Id, MapView1_Id, MapView2_Id, MapView3_Id)
+                                  VALUES(LAST_INSERT_ID(), %s, %s, %s, %s, %s, %s, %s, (SELECT max(STATIC_DATA.AISMessage_ID) FROM STATIC_DATA, AIS_MESSAGE WHERE STATIC_DATA.AISMessage_Id = AIS_MESSAGE.Id AND AIS_MESSAGE.MMSI = %s), %s, %s, %s);"""
+                        cursor.execute(stmt, (
+                            msg['Status'], msg['Position']['coordinates'][1], msg['Position']['coordinates'][0],
+                            msg['RoT'],
+                            msg['SoG'], msg['CoG'], msg['Heading'], msg['MMSI'], map_views[0], map_views[1],
+                            map_views[2]))
+                        con.commit()
+
+                    elif msg['MsgType'] == 'static_data':
+                        msg = self.format_static_data(msg)
+
+                        if msg['IMO'] is not None:
+                            stmt = """SELECT IMO FROM VESSEL WHERE IMO = %s;"""
+                            cursor.execute(stmt, (msg['IMO'],))
+                            rs = cursor.fetchone()
+                            if rs is not None:
+                                stmt = """UPDATE AIS_MESSAGE SET Vessel_IMO = %s where Id = LAST_INSERT_ID();"""
+                                cursor.execute(stmt, (msg['IMO'],))
                                 con.commit()
-                            
-                            elif msgType == 'static_data':
-                                
-                                aisimo = msg['IMO']
-                                if aisimo == 'Unknown':
-                                    aisimo = None
-                                call_sign = msg['CallSign']
-                                name = msg['Name']
-                                vessel_type = msg['VesselType']
-                                cargo_type = None
-                                length = msg['Length']
-                                breadth = msg['Breadth']
-                                draught = msg['Draught']
-                                ais_destination = None
-                                
-                                date = parser.isoparse(msg['ETA'])
-                                eta = date.strftime("%Y-%m-%d %H:%M:%S")
-                                
-                                destination_port_id = None
-                                if "Destination" in msg:
-                                    destination = msg["Destination"]
-                                
-                                stmt = """INSERT INTO static_data(AISMessage_ID, AISIMO, CallSign, Name, VesselType, CargoType, Length, Breadth, Draught, AISDestination, ETA, DestinationPort_Id)
-                                          VALUES(LAST_INSERT_ID(), %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s);""" 
-                                cursor.execute(stmt, (aisimo, call_sign, name, vessel_type, cargo_type, length, breadth, draught, ais_destination, eta, destination_port_id))
-                                con.commit()
+
+                        stmt = """INSERT INTO STATIC_DATA(AISMessage_ID, AISIMO, CallSign, Name, VesselType, CargoType, Length, Breadth, Draught, AISDestination, ETA, DestinationPort_Id)
+                                  VALUES(LAST_INSERT_ID(), %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s);"""
+                        cursor.execute(stmt, (
+                            msg['IMO'], msg['CallSign'], msg['Name'], msg['VesselType'], msg['CargoType'],
+                            msg['Length'], msg['Breadth'], msg['Draught'], msg['Destination'],
+                            msg['ETA'], msg['DestinationId']))
+                        con.commit()
+
+                    return json.dumps({"success": 1})
         except mysql.connector.Error as err:
             if err.errno == errorcode.ER_ACCESS_DENIED_ERROR:
                 print("Something is wrong with your user name or password")
@@ -111,81 +163,9 @@ class MySQL_DAO:
                 print("Database does not exist")
             else:
                 print(err)
+            return json.dumps({"success": 0})
 
-    def insert_ais_message():
-        try:
-            with MySQLConnectionManager() as con:
-                with MySQLCursorManager( con ) as cursor:
-                    with open('test_input.json', 'r') as file:
-                        data = json.load(file)
-                        for msg in data:
-                            date = parser.isoparse(msg['Timestamp'])
-                            
-                            timestamp = date.strftime("%Y-%m-%d %H:%M:%S")
-                            mmsi = msg['MMSI']
-                            vessel_class = msg['Class']
-
-                            stmt = """INSERT INTO ais_message(Timestamp, MMSI, Class) VALUES(%s, %s, %s);""" 
-                            cursor.execute(stmt, (timestamp, mmsi, vessel_class))
-                            con.commit()
-                            
-                            msgType = msg['MsgType']
-                            
-                            if msgType == 'position_report':
-                                
-                                nav_status = None
-                                if "NavigationalStatus" in msg:
-                                    nav_status = msg["NavigationalStatus"]
-                                lat = msg['Position']['coordinates'][0]
-                                long = msg['Position']['coordinates'][1]
-                                rot = msg['RoT']
-                                sog = msg['SoG']
-                                cog = msg['CoG']
-                                heading = msg['Heading']
-                                mv1 = 1
-                                mv2 = None
-                                mv3 = None
-                                
-                                stmt = """INSERT INTO position_report(AISMessage_Id, NavigationalStatus, Longitude, Latitude, RoT, SoG, CoG, Heading, LastStaticData_Id, MapView1_Id, MapView2_Id, MapView3_Id)
-                                          VALUES(LAST_INSERT_ID(), %s, %s, %s, %s, %s, %s, %s, (SELECT max(STATIC_DATA.AISMessage_ID) FROM STATIC_DATA, AIS_MESSAGE WHERE STATIC_DATA.AISMessage_Id = AIS_MESSAGE.Id AND AIS_MESSAGE.MMSI = %s), %s, %s, %s);""" 
-                                cursor.execute(stmt, (nav_status, lat, long, rot, sog, cog, heading, mmsi, mv1, mv2, mv3))
-                                con.commit()
-                            
-                            elif msgType == 'static_data':
-                                
-                                aisimo = msg['IMO']
-                                if aisimo == 'Unknown':
-                                    aisimo = None
-                                call_sign = msg['CallSign']
-                                name = msg['Name']
-                                vessel_type = msg['VesselType']
-                                cargo_type = None
-                                length = msg['Length']
-                                breadth = msg['Breadth']
-                                draught = msg['Draught']
-                                ais_destination = None
-                                
-                                date = parser.isoparse(msg['ETA'])
-                                eta = date.strftime("%Y-%m-%d %H:%M:%S")
-                                
-                                destination_port_id = None
-                                if "Destination" in msg:
-                                    destination = msg["Destination"]
-                                
-                                stmt = """INSERT INTO static_data(AISMessage_ID, AISIMO, CallSign, Name, VesselType, CargoType, Length, Breadth, Draught, AISDestination, ETA, DestinationPort_Id)
-                                          VALUES(LAST_INSERT_ID(), %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s);""" 
-                                cursor.execute(stmt, (aisimo, call_sign, name, vessel_type, cargo_type, length, breadth, draught, ais_destination, eta, destination_port_id))
-                                con.commit()
-        except mysql.connector.Error as err:
-            if err.errno == errorcode.ER_ACCESS_DENIED_ERROR:
-                print("Something is wrong with your user name or password")
-            elif err.errno == errorcode.ER_BAD_DB_ERROR:
-                print("Database does not exist")
-            else:
-                print(err)
-
-
-    def __select_all_recent_positions__(self):
+    def select_all_recent_positions(self):
         try:
             with MySQLConnectionManager() as con:
                 with MySQLCursorManager(con) as cursor:
@@ -206,14 +186,14 @@ class MySQL_DAO:
             else:
                 print(err)
 
-    def __select_most_recent_from_mmsi__(self, mmsi):  # this might have a formating err
+    def select_most_recent_from_mmsi(self, mmsi):  # this might have a formating err
         try:
             with MySQLConnectionManager() as con:
                 with MySQLCursorManager(con) as cursor:
                     # Most recent of given MMSI - CHANGE 265866000 ------------ This is probably the wrong way to implement this vv
                     cursor.execute("""SELECT t.MMSI, pos.Latitude, pos.Longitude, t.Vessel_IMO
-                                      FROM (SELECT Id, MMSI, Vessel_IMO, max(Timestamp) from AIS_MESSAGE WHERE MMSI = $mmsi) t, POSITION_REPORT as pos
-                                      WHERE t.Id = pos.AISMessage_Id;""")
+                                      FROM (SELECT Id, MMSI, Vessel_IMO, max(Timestamp) from AIS_MESSAGE WHERE MMSI = %s) t, POSITION_REPORT as pos
+                                      WHERE t.Id = pos.AISMessage_Id;""", mmsi)
                     rs = cursor.fetchone()
 
                     if rs and len(rs) > 0:
@@ -227,7 +207,7 @@ class MySQL_DAO:
             else:
                 print(err)
 
-    def __select_most_recent_5_ship_positions__(self):
+    def select_most_recent_5_ship_positions(self):
         try:
             with MySQLConnectionManager() as con:
                 with MySQLCursorManager(con) as cursor:
@@ -248,7 +228,7 @@ class MySQL_DAO:
             else:
                 print(err)
 
-    def get_tile(scale, long, lat):
+    def get_tile(self, scale, long, lat):
         """
         Get the boundaries of tile of scale 2 or 3 that contains the given position.
 
@@ -261,15 +241,18 @@ class MySQL_DAO:
         :return: object {'south': ... , 'north': ... , } describing the boundaries of the containing tile
         :rtype: dict
         """
+        SOUTH, NORTH, WEST, EAST = (54.5, 57.5, 7.0, 13.0)
         s, n, w, e = (0.0, 0.0, 0.0, 0.0)
         if scale == 1:
             s, n, w, e = (SOUTH, NORTH, WEST, EAST)
         elif scale == 2:
             s, n, w, e = math.floor(lat * 2) / 2, math.ceil(lat * 2) / 2, math.floor(long), math.ceil(long)
         elif scale == 3:
-            s, n, w, e = math.floor(lat * 4) / 4, math.ceil(lat * 4) / 4, math.floor(long * 2) / 2, math.ceil(long * 2) / 2
+            s, n, w, e = math.floor(lat * 4) / 4, math.ceil(lat * 4) / 4, math.floor(long * 2) / 2, math.ceil(
+                long * 2) / 2
 
         return {'south': s, 'north': n, 'west': w, 'east': e}
+
 
 # vvv --- TO DO --- vvv
 
